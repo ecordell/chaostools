2a431c5a98b2ab20316f7c3aad02f7bf4820a81a
Evan Cordell <cordell.evan@gmail.com>
2009-08-19 22:20:32 -0500

    Beginning to refactor.

---
+++ src/ifsPanel.h
@@ -0,0 +1,13 @@
+#ifndef IFSPANEL_H
+#define IFSPANEL_H
+
+class ifsPanel : public wxPanel
+{
+public:
+	DECLARE_DYNAMIC_CLASS(ifsPanel);
+	
+	ifsPanel() {}
+	ifsPanel(wxWindow* parent, wxW//finish
+};
+
+#endif
